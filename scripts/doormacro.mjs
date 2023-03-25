d42da1aaf3fa3af58924fac63b0b3ac118c496e5
Kenneth B <kennethzhell@gmail.com>
2023-03-25 15:29:56 +0100

    Clean up some code and add a tooltip

--- scripts/doormacro.mjs
+++ scripts/doormacro.mjs
@@ -1,17 +1,14 @@
-import { MODULE, TRIGGERS } from "./constants.mjs";
-
-export function renderDoorMacroConfig(wallDocument) {
-  new DoorMacroConfig(wallDocument, {}).render(true);
-}
+import {MODULE, TRIGGERS} from "./constants.mjs";
 
 /**
  * Execute macros.
- * @param wallDoc   The door.
- * @param whenWhat  The trigger.
- * @param gmId      The first active GM found.
- * @param userId    The id of the user who changed the door.
+ * @param {WallDocument} wallDoc      The door.
+ * @param {string} whenWhat           The trigger.
+ * @param {object} context            An object of user ids.
+ * @param {string} context.gmId       The first active GM found.
+ * @param {string} context.userId     The id of the user who changed the door.
  */
-export function callMacro(wallDoc, whenWhat, { gmId, userId }) {
+export function callMacro(wallDoc, whenWhat, {gmId, userId}) {
   const script = wallDoc.getFlag(MODULE, `commands.${whenWhat}`);
   const asGM = wallDoc.getFlag(MODULE, `asGM.${whenWhat}`);
   if (!script) return;
@@ -26,39 +23,50 @@ export function callMacro(wallDoc, whenWhat, { gmId, userId }) {
 }
 
 export class DoorMacroConfig extends MacroConfig {
+
+  /** @override */
   constructor(wallDocument, options) {
     super(wallDocument, options);
     this.isSecret = wallDocument.door === CONST.WALL_DOOR_TYPES.SECRET;
   }
 
+  /** @override */
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       template: "modules/doormacro/templates/doormacro.hbs",
       classes: ["macro-sheet", "sheet", MODULE],
-      tabs: [{ navSelector: ".tabs", contentSelector: ".content-tabs", initial: "whenCreated" }],
+      tabs: [{navSelector: ".tabs", contentSelector: ".content-tabs", initial: "whenCreated"}],
       height: 600,
       width: 600
     });
   }
 
+  /**
+   * Override for the sheet id.
+   * @returns {string}      The id of the application.
+   */
   get id() {
     return `doormacro-${this.object.id}`;
   }
 
+  /** @override */
   async getData() {
     const data = await super.getData();
-    data.name = `Door: ${this.object.id}`;
+    data.name = game.i18n.format("DOORMACRO.Door", {id: this.object.id});
+    const commands = this.object.flags[MODULE]?.commands ?? {};
+    const gms = this.object.flags[MODULE]?.asGM ?? {};
     data.triggers = TRIGGERS.map(trigger => {
       return {
         type: trigger,
-        command: this.object.getFlag(MODULE, `commands.${trigger}`),
-        asGM: this.object.getFlag(MODULE, `asGM.${trigger}`),
+        command: commands[trigger],
+        asGM: gms[trigger],
         label: game.i18n.localize(`DOORMACRO.${trigger}`)
       }
     });
     return data;
   }
 
+  /** @override */
   async _updateObject(event, formData) {
     for (const trigger of TRIGGERS) {
       if (!formData[`flags.${MODULE}.commands.${trigger}`]) {
--- setup.mjs
+++ setup.mjs
@@ -1,5 +1,5 @@
-import { MODULE, TRIGGERS } from "./scripts/constants.mjs";
-import { callMacro, renderDoorMacroConfig } from "./scripts/doormacro.mjs";
+import {MODULE, TRIGGERS} from "./scripts/constants.mjs";
+import {callMacro, DoorMacroConfig} from "./scripts/doormacro.mjs";
 
 // Create a button in a door's header.
 Hooks.on("getWallConfigHeaderButtons", (config, buttons) => {
@@ -9,7 +9,7 @@ Hooks.on("getWallConfigHeaderButtons", (config, buttons) => {
   buttons.unshift({
     class: MODULE,
     icon: "fa-solid fa-door-open",
-    onclick: () => renderDoorMacroConfig(config.object)
+    onclick: () => new DoorMacroConfig(config.object).render(true)
   });
 });
 
@@ -21,7 +21,7 @@ Hooks.once("setup", () => {
 
 // save previous state of door.
 Hooks.on("preUpdateWall", (wallDoc, update, context, userId) => {
-  const { WALL_DOOR_STATES: DS, WALL_DOOR_TYPES: DOOR } = CONST;
+  const {WALL_DOOR_STATES: DS, WALL_DOOR_TYPES: DOOR} = CONST;
 
   // ignore this if the Door type involves toggling between wall and door.
   if (wallDoc.door === DOOR.NONE || update.door === DOOR.NONE) return;
@@ -31,23 +31,23 @@ Hooks.on("preUpdateWall", (wallDoc, update, context, userId) => {
 
   context[MODULE] = {
     [wallDoc.id]: {
-      whenClosed: wallDoc.ds === DS.OPEN && (hasDS && update.ds === DS.CLOSED),
-      whenOpened: wallDoc.ds !== DS.OPEN && (hasDS && update.ds === DS.OPEN),
-      whenHidden: wallDoc.door !== DOOR.SECRET && (hasDOOR && update.door === DOOR.SECRET),
-      whenRevealed: wallDoc.door === DOOR.SECRET && (hasDOOR && update.door !== DOOR.SECRET),
-      whenLocked: wallDoc.ds !== DS.LOCKED && (hasDS && update.ds === DS.LOCKED),
-      whenUnlocked: wallDoc.ds === DS.LOCKED && (hasDS && update.ds !== DS.LOCKED)
+      whenClosed: (wallDoc.ds === DS.OPEN) && (hasDS && (update.ds === DS.CLOSED)),
+      whenOpened: (wallDoc.ds !== DS.OPEN) && (hasDS && (update.ds === DS.OPEN)),
+      whenHidden: (wallDoc.door !== DOOR.SECRET) && (hasDOOR && (update.door === DOOR.SECRET)),
+      whenRevealed: (wallDoc.door === DOOR.SECRET) && (hasDOOR && (update.door !== DOOR.SECRET)),
+      whenLocked: (wallDoc.ds !== DS.LOCKED) && (hasDS && (update.ds === DS.LOCKED)),
+      whenUnlocked: (wallDoc.ds === DS.LOCKED) && (hasDS && (update.ds !== DS.LOCKED))
     }
   }
 });
 
 Hooks.on("updateWall", (wallDoc, update, context, userId) => {
-  const { id: gmId } = game.users.find(user => {
+  const {id: gmId} = game.users.find(user => {
     return user.active && user.isGM;
   }) ?? {};
   TRIGGERS.map(trigger => {
     const property = `${MODULE}.${wallDoc.id}.${trigger}`;
     const triggered = foundry.utils.getProperty(context, property);
-    if (triggered) callMacro(wallDoc, trigger, { gmId, userId });
+    if (triggered) callMacro(wallDoc, trigger, {gmId, userId});
   });
 });
